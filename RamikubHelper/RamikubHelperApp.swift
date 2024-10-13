b918be45502a65d530527d6622e6d96287578073
Fernando Mousinho <fmousinho76@gmail.com>
2024-10-13 12:08:20 -0500

    Initial Commit

---
+++ RamikubHelper/RamikubHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamikubHelperApp.swift
+//  RamikubHelper
+//
+//  Created by Fernando Mousinho on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RamikubHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RamikubHelperTests/RamikubHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  RamikubHelperTests.swift
+//  RamikubHelperTests
+//
+//  Created by Fernando Mousinho on 10/13/24.
+//
+
+import Testing
+@testable import RamikubHelper
+
+struct RamikubHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
