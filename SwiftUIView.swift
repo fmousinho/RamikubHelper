c3063fada7b43ca5a0f36f2bc25ecd213a7f604e
Fernando Mousinho <fmousinho76@gmail.com>
2024-10-29 17:19:04 -0500

    Normal update

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RamikubHelper
+//
+//  Created by Fernando Mousinho on 10/29/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
