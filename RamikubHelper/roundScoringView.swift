314f65fa8389e02af204e8d6a5ea264f8b23d428
Fernando Mousinho <fmousinho76@gmail.com>
2024-11-04 15:40:45 -0600

    Working version

---
+++ RamikubHelper/roundScoringView.swift
@@ -0,0 +1,18 @@
+//
+//  roundScoringView.swift
+//  RamikubHelper
+//
+//  Created by Fernando Mousinho on 11/2/24.
+//
+
+import SwiftUI
+
+struct roundScoringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    roundScoringView()
+}
